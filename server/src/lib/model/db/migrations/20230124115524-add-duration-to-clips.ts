11d1edafd0a155d23ca1e946a4032ecb040496a8
SergioNaranjo100 <129307616+HalbaECSergio@users.noreply.github.com>
2023-04-14 07:58:48 -0500

    Update Base de datos erorr
    modificar el archivo de migracion de clips

--- server/src/lib/model/db/migrations/20230124115524-add-duration-to-clips.ts
+++ server/src/lib/model/db/migrations/20230124115524-add-duration-to-clips.ts
@@ -1,19 +1,7 @@
 export const up = async function (db: any): Promise<any> {
   return db.runSql(`
-    IF NOT EXISTS (
-      SELECT
-        NULL
-      FROM
-        INFORMATION_SCHEMA.COLUMNS
-      WHERE
-        TABLE_NAME = 'clips'
-        AND COLUMN_NAME = 'duration' 
-      ) THEN
-      ALTER TABLE clips ADD COLUMN duration INT NOT NULL DEFAULT 0,
-      ALGORITHM = INPLACE,
-      LOCK = NONE;
-      END IF;
-    `);
+      CALL modificarTablaClips();
+  `)
 };
 
 export const down = async function (db: any): Promise<any> {
